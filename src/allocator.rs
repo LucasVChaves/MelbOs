60f8ed1943eaed1698a6a7e0be37f408193eeebe
Lucas <y3tsec@protonmai..com>
2021-09-22 22:19:01 -0300

    Finished the memory system.
    Bump, linked list and Fixed-size block allocators are ready, and their
    tests too. All memory system is complete and tested for now. No bugs
    found yet.

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,8 +1,6 @@
-use spin::Mutex;
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
-use linked_list_allocator::LockedHeap;
-use bump::BumpAllocator;
+use fixed_size_block::FixedSizeBlockAllocator;
 use x86_64::{
     structures::paging::{
         mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
@@ -11,15 +9,14 @@ use x86_64::{
 };
 
 pub mod bump;
+pub mod fixed_size_block;
 pub mod linked_list;
-
-pub struct Dummy;
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
 
 pub fn init_heap(
     mapper: &mut impl Mapper<Size4KiB>,
@@ -34,12 +31,11 @@ pub fn init_heap(
     };
 
     for page in page_range {
-        let frame = frame_allocator.allocate_frame()
+        let frame = frame_allocator
+            .allocate_frame()
             .ok_or(MapToError::FrameAllocationFailed)?;
         let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
-        unsafe {
-            mapper.map_to(page, frame, flags, frame_allocator)?.flush()
-        };
+        unsafe { mapper.map_to(page, frame, flags, frame_allocator)?.flush() };
     }
 
     unsafe {
@@ -49,33 +45,38 @@ pub fn init_heap(
     Ok(())
 }
 
+pub struct Dummy;
+
 unsafe impl GlobalAlloc for Dummy {
     unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
         null_mut()
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
-        panic!("dealloc should never be called")
+        panic!("dealloc should be never called")
     }
 }
 
-/// A wraper around spin::Mutex to permit trait implementations.
+/// A wrapper around spin::Mutex to permit trait implementations.
 pub struct Locked<A> {
     inner: spin::Mutex<A>,
 }
 
 impl<A> Locked<A> {
     pub const fn new(inner: A) -> Self {
-        Locked{
+        Locked {
             inner: spin::Mutex::new(inner),
         }
     }
-    
+
     pub fn lock(&self) -> spin::MutexGuard<A> {
         self.inner.lock()
     }
 }
 
+/// Align the given address `addr` upwards to alignment `align`.
+///
+/// Requires that `align` is a power of two.
 fn align_up(addr: usize, align: usize) -> usize {
     (addr + align - 1) & !(align - 1)
 }
